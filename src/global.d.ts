053e3acd71a96184306393b7374d576b8f4ce2a0
Moraru <tiltiobusiness@gmail.com>
2024-05-22 11:16:40 +0300

    removed comments

--- src/global.d.ts
+++ src/global.d.ts
@@ -12,8 +12,3 @@ declare module "*.env" {
   const content: { [className: string]: string };
   export default content;
 }
-
-// declare module '*.module.css' {
-//   const classes: { [key: string]: string };
-//   export default classes;
-// }
